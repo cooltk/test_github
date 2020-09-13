44d3b0e964464a52d1c331058e61688ae60ab6d1
Nicolas Mauvilain <M226323@eu.merckgroup.com>
2020-09-13 19:12:52 +0200

    test validation

--- test_github/Program.cs
+++ test_github/Program.cs
@@ -7,6 +7,7 @@ namespace test_github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello2");
         }
     }
 }
