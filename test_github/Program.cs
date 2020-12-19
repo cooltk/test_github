735c7e23093b3860205617903000d00dc2bbacf7
Nicolas Mauvilain <M226323@eu.merckgroup.com>
2020-12-19 23:22:06 +0100

    Test commit depuis visual studio

--- test_github/Program.cs
+++ test_github/Program.cs
@@ -7,7 +7,7 @@ namespace test_github
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello5");
+            Console.WriteLine("Hello6");
         }
     }
 }
